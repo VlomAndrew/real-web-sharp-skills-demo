608e746adc90c00b0bf0252d754b244e3126c949
VlomAndrew <andrej_karpov_98@mail.ru>
2021-01-15 17:03:09 +0300

    создана первая версия мат калькулятора

---
+++ MainWebApp/Controllers/CalcController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MainWebApp.Controllers
+{
+    public class CalcController : Controller
+    {
+        // GET: Calc
+        public ActionResult Index(int? result)
+        {
+
+            ViewBag.result = result ?? 0;
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Calc(string expression)
+        {
+            int? res = null;
+            try
+            {
+                 res = (int) (new DataTable().Compute(expression, null));
+            }
+            catch (Exception e)
+            {
+                
+            }
+
+            return RedirectToAction("Index", new {result = res});
+        }
+    }
+}
